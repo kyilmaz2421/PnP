beec16fe129012949014433d99a5d3c80ed489de
erik <erikahndersun@gmail.com>
2018-11-17 16:14:47 -0500

    Started to experiment with parsing querystring

--- php/sign_up_page.php
+++ php/sign_up_page.php
@@ -7,28 +7,31 @@ $password = "pnpdbpassword1";
 // querys string field vars
 $unameEntry = "";
 $passwordEntry="";
+$loginCredentialsArray = [];
 
-echo implode(" ", $_SERVER);
+// put username and password entered into loginCredArr
+echo $_SERVER['QUERY_STRING'];
+parse_str($_SERVER['QUERY_STRING'], $loginCredentialsArray);
+print_r($loginCredentialsArray);
 
+$unameEntry = $loginCredentialsArray['username'];
+$passwordEntry = $loginCredentialsArray['password'];
 
 try {
-
+	    
+	    // connect to db
 	    $conn = new PDO("mysql:host=$servername;dbname=pnpdb", $usernamedb, $password);
 	    // set the PDO error mode to exception
 	    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	    echo "Connected successfully"; 
-		
+
+	    // query db if it has user's entries	    
 	    $dbUsername = $conn->query("SELECT Username FROM Users");
 	    $dbPassword = $conn->query("SELECT Password FROM Users");
 
 		var_dump($dbUsername);
 
-	    $stmt->execute();
 		
-	    $result = $stmt->setFetchMode(PDO::FETCH_ASSOC); 
-    		foreach(new TableRows(new RecursiveArrayIterator($stmt->fetchAll())) as $k=>$v) { 
-        		echo $v;
-   		 }
 	   
     }
 catch(PDOException $e)
