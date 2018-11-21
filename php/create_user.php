12c98216a7a37170862a046205aa51fdc5e6d6e9
kyilmaz2421 <kaanyilmaz@ymail.com>
2018-11-21 18:57:55 -0500

    create_user.php is built but still needs checks for incorrect entries (duplicate entries)

--- php/create_user.php
+++ php/create_user.php
@@ -10,24 +10,13 @@ try {
 	    $conn = new PDO("mysql:host=$servername;dbname=pnpdb", $usernamedb, $password);
 	    // set the PDO error mode to exception
 	    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	    echo "Connected successfully"; 
 
-	    $sql = "INSERT INTO Places VALUES ('{$usernamePerson}', '{$_POST["building"]}', '{$_POST["adr"]}','{$_POST["aptNum"]}','{$_POST["city"]}','{$_POST["province"]}','{$_POST["country"]}','{$_POST["postalCode"]}','{$_POST["spaceType"]}','{$_POST["description"]}','{$_POST["price"]}','0','{$_POST["pets"]}', '{$_POST["alcohol"]}','{$_POST["wheelchair"]}','{$_POST["smoking"]}','{$_POST["outdoors"]}', '1')";
-	    
-	    $conn->exec($sql);
-
-	    $stmt = $conn->prepare("SELECT * FROM Places");
-
-	    $stmt->execute();
-		
-	    $result = $stmt->setFetchMode(PDO::FETCH_ASSOC); 
-    		foreach(new TableRows(new RecursiveArrayIterator($stmt->fetchAll())) as $k=>$v) { 
-        		echo $v;
-   		 }
-	   
+	    $sql = "INSERT INTO Places VALUES ('{$usernamePerson}', '{$_POST["firstname"]}', '{$_POST["lastname"]}','{$_POST["email"]}','{$_POST["password"]}','{$_POST["tel"]}','0','0','{$_POST["gender"]}','{$_POST["info"]}','{$_POST["bday"]}','0')";
+		header('Location: http://localhost/pnp/viewingPage.html');
+		exit();
     }
 catch(PDOException $e)
     {
-    	exit();
+    	echo "Connection failed: " . $e->getMessage();
     }
 ?>
