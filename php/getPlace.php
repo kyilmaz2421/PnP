6e4188cdd16db3b48a6350c738c62eefbc3739fd
alana-ceci <alana.ceci@mail.mcgill.ca>
2018-11-24 16:37:21 -0500

    merging to pull

--- php/getPlace.php
+++ php/getPlace.php
@@ -2,19 +2,33 @@
 $servername = "localhost";
 $usernamedb = "root";
 $password = "pnpdbpassword1";
-$usernamePerson = "dgand";
-$placeId = "995629BC7AF715D9A88B35FB28EE18D23C87F1AF707E1F685EC100839CFC84D9";
-if( isset( $_POST['space'] ) )
-{
-echo '  <section class="placeContainer">
+$usernamePerson = "cc98";
+try {
+
+	    $conn = new PDO("mysql:host=$servername;dbname=pnpdb", $usernamedb, $password);
+	    // set the PDO error mode to exception
+	    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        
+        echo  $space = $_POST['space'];
+        echo $price= $_POST['price'];
+        echo  $rating = $_POST['rating'];
+        echo  $pets = $_POST['pets'];
+        $alcohol = $_POST['alcohol'];
+        $wheelchair = $_POST['wheelchair'];
+        $smoking = $_POST['smoking'];
+        $outdoor= $_POST['outdoor'];
+        $stmt = $conn->prepare('SELECT * FROM Places WHERE TypeOfSpace = :space AND PricePerNight = :price AND Rating = :rating AND Pets = :pets AND Alcohol= :alcohol AND Wheelchair= :wheelchair AND OutdoorAccess= :outdoor');
+        $stmt->execute(['space' => $space, 'price' => $price, 'rating' => $rating, 'pets' => $pets, 'alcohol' => $alcohol, 'wheelchair' => $wheelchair, 'outdoor' => $outdoor]);
+		$result = $stmt->fetch(PDO::FETCH_ASSOC);
+        echo '  <section class="placeContainer">
                 <div id="placeImage">
                         <img id= "image" src="img/house.jpeg" alt="House">
                 </div>
             
                 <div id="details">
                     <div>Address:</div>
-                    <div>Description:</div>
-                    <div>Price per night:</div>
+                    <div>Description: $space </div>
+                    <div>Price per night: $price </div>
                 </div>
             
                 <div id = "userInfo">
@@ -22,7 +36,7 @@ echo '  <section class="placeContainer">
                             User:
                     </div>
                     <div id = "rating">
-                            Rating: 
+                            Rating: $rating
                     </div>
                 </div>
             </section>
@@ -31,25 +45,6 @@ echo '  <section class="placeContainer">
             </form>
         </section>
 ';
-}
-else{
-    echo 'fuck';
-}
-try{ 
-    
-    $conn = new PDO("mysql:host=$servername;dbname=pnpdb", $usernamedb, $password);
-    // set the PDO error mode to exception
-
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $stmt = $conn->prepare('SELECT * FROM Places WHERE TypeOfSpace= :space AND PlaceID = :placeId');
-    $stmt->execute(['username' => $usernamePerson, 'placeId' => $placeId]);
-    $result = $stmt->fetch(PDO::FETCH_ASSOC);
-    
-    if($result === False){
-    echo "Query doesnt exist";
-    }
-
-    print_r($result['Desciption']);
 }
 
 catch(PDOException $e){
