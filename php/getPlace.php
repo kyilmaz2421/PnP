683dbb87781b524519ccfd3fd4226bc828b33757
erik <erikahndersun@gmail.com>
2018-11-24 16:19:26 -0500

    cleaned up comments

--- php/getPlace.php
+++ php/getPlace.php
@@ -5,6 +5,7 @@ $password = "pnpdbpassword1";
 $usernamePerson = "dgand";
 $placeId = "995629BC7AF715D9A88B35FB28EE18D23C87F1AF707E1F685EC100839CFC84D9";
 
+    // handle error thrown for dev
     if(isset($_SERVER['REQUEST_METHOD'])) {
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $space = $_POST['spaceType'];
@@ -16,58 +17,31 @@ $placeId = "995629BC7AF715D9A88B35FB28EE18D23C87F1AF707E1F685EC100839CFC84D9";
             $smoking = $_POST["smoking"];
             $outdoors = $_POST["outdoors"];
 
-            print_r ($pets . "somethin " . $rating . " some " . $alc ." as");
-
         try{ 
         
-        $conn = new PDO("mysql:host=$servername;dbname=pnpdb", $usernamedb, $password);
-        // set the PDO error mode to exception
+            $conn = new PDO("mysql:host=$servername;dbname=pnpdb", $usernamedb, $password);
+            // set the PDO error mode to exception
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        // $stmt = $conn->prepare('SELECT * FROM Places WHERE TypeOfSpace= :space AND PricePerNight = :price AND Rating >= :rating  AND Pets = :pets  AND Alcohol = :alcohol  AND Wheelchair = :wheelchair  AND Smoking = :smoking  AND OutdoorAccess = :outdoors');
+            // Prepare the sql statement
+            // keep in mind this is a security flaw
+            // !!! fix before release TODO
+            $s0 = "SELECT * FROM Places WHERE TypeOfSpace ='" . $space . "'" ;
+            $s1 = " AND PricePerNight <=". $price . "";
+            $s2 = " AND Pets =" . $pets . "";
+            $s3 = " AND Alcohol =" . $alc . "";
+            $s4 = " AND Wheelchair =" . $wheelchair . "";
+            $s5 = " AND Smoking =" . $smoking . "";
+            $s6 = " AND OutdoorAccess =" . $outdoors . "";
 
-        // $stmt->execute([':space' => $space, 'price' => $price, 'rating' => $rating, 'pets' => $pets, 'alcohol' => $alc, 'wheelchair' => $wheelchair, 'smoking' => $smoking, 'outdoors' => $outdoors]);
+            $sql = $s0 . $s1 . $s2 . $s3 . $s4 . $s5 . $s6;
 
-        // $stmt = $conn->prepare('SELECT * FROM Places WHERE TypeOfSpace = :space');
-        // $stmt->execute(['space' => "Home"]);
+          
+            // Get result set from db
+            $result = $conn->query($sql)->fetchAll(PDO::FETCH_ASSOC);
 
-        // $result = $stmt->fetch(PDO::FETCH_ASSOC);
-
-        // // if result set not returned
-        // if($result === null){
-        //     // print_r ($stmt . "    ");
-        //     print_r (gettype($stmt));
-        //     echo "queryDNE";
-
-        // } else {
-        //     print_r(gettype($result));
-        //     var_dump($result);
-        //     echo " ";
-        //     echo($result['smoking']);
-        // }
-
-        // Most of the db fields in this query are tiny in hence no single quotes
-        // 
-
-        $s0 = "SELECT * FROM Places WHERE TypeOfSpace ='" . $space . "'" ;
-        $s1 = " AND PricePerNight <=". $price . "";
-        $s2 = " AND Pets =" . $pets . "";
-        $s3 = " AND Alcohol =" . $alc . "";
-        $s4 = " AND Wheelchair =" . $wheelchair . "";
-        $s5 = " AND Smoking =" . $smoking . "";
-        $s6 = " AND OutdoorAccess =" . $outdoors . "";
-
-        $sql = $s0 . $s1 . $s2 . $s3 . $s4 . $s5 . $s6;
-
-        // $result = $conn->query("SELECT * FROM Places WHERE TypeOfSpace ='" . $space . "'" AND PricePerNight ="" . $price . "" AND Pets ="" . $pets . "" AND Alcohol = "" . $alc . "" AND Wheelchair = "" . $wheelchair . "" AND Smoking = "" . $smoking . "" AND OutdoorAccess = "" . $outdoors . "")->fetchAll(PDO::FETCH_ASSOC);
-        print_r ($sql);
-        $result = $conn->query($sql)->fetchAll(PDO::FETCH_ASSOC);
-
-        print_r ($result);
-        var_dump($result);
-        echo 'here';
-
-        
+            // display search result for user
+            print_r ($result);
 
         } catch(PDOException $e) {
             echo $e;
