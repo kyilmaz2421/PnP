9c41436c6188ab55127ae30305c9ea61ea3c9a70
kyilmaz2421 <kaanyilmaz@ymail.com>
2018-11-23 23:45:04 -0500

    finally fixed  ajax calls just need to build the search query

--- php/getPlace.php
+++ php/getPlace.php
@@ -4,47 +4,30 @@ $usernamedb = "root";
 $password = "pnpdbpassword1";
 $usernamePerson = "dgand";
 $placeId = "995629BC7AF715D9A88B35FB28EE18D23C87F1AF707E1F685EC100839CFC84D9";
-if( isset( $_POST['space'] ) )
-{
-echo '  <section class="placeContainer">
-                <div id="placeImage">
-                        <img id= "image" src="img/house.jpeg" alt="House">
-                </div>
-            
-                <div id="details">
-                    <div>Address:</div>
-                    <div>Description:</div>
-                    <div>Price per night:</div>
-                </div>
-            
-                <div id = "userInfo">
-                    <div id = "user">  
-                            User:
-                    </div>
-                    <div id = "rating">
-                            Rating: 
-                    </div>
-                </div>
-            </section>
-            <form  method="POST" action="http://localhost/pnp/php/getPlace.php">
-                <button type="submit"  name= "test" id = "bookPlace"> Book </button>
-            </form>
-        </section>
-';
-}
-else{
-    echo 'fuck';
-}
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // $space = $_POST['spaceType'];
+    // $price = intval($_POST["price"]); 
+    // $rating = floatval($_POST["rating"]);
+    // $pets = (bool)$_POST["pets"];
+    // $alc = (bool)$_POST["alcohol"];
+    // $wheelchair = (bool)$_POST["wheelchair"];
+    // $smoking = (bool)$_POST["smoking"];
+    // $outdoors = (bool)$_POST["outdoors"];
+
+
 try{ 
     
     $conn = new PDO("mysql:host=$servername;dbname=pnpdb", $usernamedb, $password);
     // set the PDO error mode to exception
 
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $stmt = $conn->prepare('SELECT * FROM Places WHERE TypeOfSpace= :space AND PlaceID = :placeId');
-    $stmt->execute(['username' => $usernamePerson, 'placeId' => $placeId]);
+   // $stmt = $conn->prepare('SELECT * FROM Places WHERE TypeOfSpace= :space AND PricePerNight = :price AND Rating >= :rating  AND Pets = :pets  AND Alcohol = :alcohol  AND Wheelchair = :wheelchair  AND Smoking = :smoking  AND OutdoorAccess = :outdoors');
+   //$stmt->execute(['space' => $space, 'price' => $price, 'rating' => $rating, 'pets' => $pets, 'alcohol' => $alc, 'wheelchair' => $wheelchair, 'smoking' => $smoking, 'outdoors' => $outdoors]);
+   $stmt = $conn->prepare('SELECT * FROM Places WHERE TypeOfSpace = :space');
+   $stmt->execute(['space' => "Home"]);
     $result = $stmt->fetch(PDO::FETCH_ASSOC);
-    
+
     if($result === False){
     echo "Query doesnt exist";
     }
@@ -56,6 +39,6 @@ catch(PDOException $e){
     echo "        u       ck";
     exit();
 }
-
+}
 
 ?>
