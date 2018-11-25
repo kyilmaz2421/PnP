d33308a54bd60649d3ea4209f82b87cb4d207ca4
erik <erikahndersun@gmail.com>
2018-11-25 15:35:35 -0500

    Please stay the same

--- php/getPlace.php
+++ php/getPlace.php
@@ -9,6 +9,7 @@ $placeId = "995629BC7AF715D9A88B35FB28EE18D23C87F1AF707E1F685EC100839CFC84D9";
     if(isset($_SERVER['REQUEST_METHOD'])) {
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $space = $_POST['spaceType'];
+
             $price = intval($_POST["price"]); 
             $rating = floatval($_POST["rating"]);
             $pets = $_POST["pets"];
@@ -27,16 +28,22 @@ $placeId = "995629BC7AF715D9A88B35FB28EE18D23C87F1AF707E1F685EC100839CFC84D9";
             // keep in mind this is a security flaw
             // !!! fix before release TODO
 
-            
-            $s0 = "SELECT * FROM Places WHERE TypeOfSpace ='" . $space . "'" ;
-            $s1 = " AND PricePerNight <=". $price . "";
-            $s2 = " AND Pets =" . $pets . "";
-            $s3 = " AND Alcohol =" . $alc . "";
-            $s4 = " AND Wheelchair =" . $wheelchair . "";
-            $s5 = " AND Smoking =" . $smoking . "";
-            $s6 = " AND OutdoorAccess =" . $outdoors . "";
+            $sql = "";
+            if($space === "0") {
+                $sql = "SELECT * FROM Places";
+            } else {
+                 $s0 = "SELECT * FROM Places WHERE TypeOfSpace ='" . $space . "'" ;
+                $s1 = " AND PricePerNight <=". $price . "";
+                $s2 = " AND Pets =" . $pets . "";
+                $s3 = " AND Alcohol =" . $alc . "";
+                $s4 = " AND Wheelchair =" . $wheelchair . "";
+                $s5 = " AND Smoking =" . $smoking . "";
+                $s6 = " AND OutdoorAccess =" . $outdoors . "";
 
-            $sql = $s0 . $s1 . $s2 . $s3 . $s4 . $s5 . $s6;
+                $sql = $s0 . $s1 . $s2 . $s3 . $s4 . $s5 . $s6;
+            }
+            
+           
 
           
             // Get result set from db
