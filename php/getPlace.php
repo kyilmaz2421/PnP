374a9b5fcff3cdcaaf2260ba2ac87b61ba28e6d7
alana-ceci <alana.ceci@mail.mcgill.ca>
2018-11-30 15:28:20 -0500

    fixed big bug with echoing HTML, places now displaying from database onto page.

--- php/getPlace.php
+++ php/getPlace.php
@@ -1,20 +1,17 @@
 <?php
 // session_start();
 	include("../php/session.php");
-	echo ($_SESSION['login_user']);
-?>
-<?php
+   // echo ($_SESSION['login_user']);
+// phpinfo();
+
 $servername = "localhost";
 $usernamedb = "root";
 $password = "pnpdbpassword1";
 $usernamePerson = $_SESSION['login_user'];
-
-
     // handle error thrown for dev
     if(isset($_SERVER['REQUEST_METHOD'])) {
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            $space = $_POST['spaceType'];
-
+            $space = strval($_POST["spaceType"]);
             $price = intval($_POST["price"]); 
             $rating = floatval($_POST["rating"]);
             $pets = $_POST["pets"];
@@ -37,8 +34,8 @@ $usernamePerson = $_SESSION['login_user'];
             if($space === "0") {
                 $sql = "SELECT * FROM Places";
             } else {
-                 $s0 = "SELECT * FROM Places WHERE Username not = '" . $usernamePerson . "'" ;
-                $s1 = "AND TypeOfSpace ='" . $space . "'" ; 
+                $s0 = "SELECT * FROM Places WHERE" ;
+                $s1 = "TypeOfSpace ='" . $space . "'" ; 
                 $s2 = " AND PricePerNight <=". $price . "";
                 $s3 = " AND Pets =" . $pets . "";
                 $s4 = " AND Alcohol =" . $alc . "";
@@ -48,75 +45,66 @@ $usernamePerson = $_SESSION['login_user'];
 
                 $sql = $s0 . $s1 . $s2 . $s3 . $s4 . $s5 . $s6 . $s7;
             }
-            
-           
-
-          
             // Get result set from db
             $result = $conn->query($sql)->fetchAll(PDO::FETCH_ASSOC);
 
-            // display search result for user
-            print_r ($result);
-<<<<<<< HEAD
-            // if(count($result) == 0){
-            //     echo "No results match your search...party pooper :(";
-            // }
-            // for($x=0, $n=count($result); $x<$n; $x++){
-            //     echo "HEY";
-=======
-
-        //     if(count($result) == 0){
-        //         echo "No results match your search...party pooper :(";
-        //     }
-        //     for($x=0, $n=count($result); $x<$n; $x++){
-        //         echo
->>>>>>> 132b5ffacbb9d154522eaf38d1a2166c35d03fef
-        //             '<section class="placeContainer"
-		// 				<div id="placeImage">
-		// 					img!
-		// 				</div>
-		// 				<div class="details">
-<<<<<<< HEAD
-		// 					<div> Address: $result[$x]["Address"] </div>
-=======
-		// 					<div> Address: {$result[$x]["Address"]} </div>
->>>>>>> 132b5ffacbb9d154522eaf38d1a2166c35d03fef
-		// 					<div>Description: $result[$x]["Desciption"]
-		// 					</div>
-		// 					<div>Price per night: $result[$x]["PricePerNight"]</div>
-		// 				</div>
-					
-		// 				<div class = "userInfo">
-		// 					<div>  
-		// 							User: $result[$x]["FirstName"] $result[$x]["LastName"]
-		// 							Rating: $result[$x]["Rating"]
-		// 					</div>
-		// 				</div>            
-							
-		// 				<div class="bookButton">
-		// 					<button id = bookButton onclick = "showBooking()"> Book </button>
-		// 						<div id= "book" style = "display: none">
-		// 							<form method = "POST" action = "php/book_place.php">
-		// 								Preferred Date:
-		// 							<input type= "date" id="booking" name="bookDate" required>
-		// 								<button type = "submit" id = "submitDate"> Check Date </button>
-		// 							</form>
-		// 						</div>
-		// 				</div>
-		// 			</section>
-
-		// </section>';
-<<<<<<< HEAD
-            //}
-=======
-        //     }
->>>>>>> 132b5ffacbb9d154522eaf38d1a2166c35d03fef
-
+             if(count($result) == 0){
+              echo "No results match your search...party pooper :(";
+             }
+          for($x=0, $n=count($result); $x<$n; $x++){
+              echo '<section class="placeContainer"
+				<div id="placeImage">
+					img!
+					</div>
+						<div class="details">
+							<div> Address: '. $result[$x]["StreetName"] . ', ' . $result[$x]["City"]  .  ',  ' .$result[$x]["Province"]. '</div>
+						<div>Description: '. $result[$x]["Desciption"] .'
+						</div>
+					<div>Price per night:  $ '. $result[$x]["PricePerNight"] .' CAD </div>
+                    </div> 
+                    <div class = "userInfo">
+                        <div>  
+                                User: aceci
+                                Rating: 5
+                        </div>
+                    </div>            
+                        
+                    <div class="bookButton">
+                        <button id = bookButton onclick = \'showBooking()\'> Book </button>
+                            <div id= "book" style = "display: none">
+                                <form method = "POST" action = "php/book_place.php">
+                                    Preferred Date:
+                                <input type= "date" id="booking" name="bookDate" required>
+                                    <button type = "submit" id = "submitDate"> Check Date </button>
+                                </form>
+                            </div>
+                    </div>
+                </section>';
+				
+            } 
+         //  myprint_r($result);
+            
         } catch(PDOException $e) {
             echo $e;
             exit();
         }
     }
+}
+
+function myprint_r($my_array) {
+    if (is_array($my_array)) {
+        echo "<table border=1 cellspacing=0 cellpadding=3 width=100%>";
+        echo '<tr><td colspan=2 style="background-color:#333333;"><strong><font color=white>ARRAY</font></strong></td></tr>';
+        foreach ($my_array as $k => $v) {
+                echo '<tr><td valign="top" style="width:40px;background-color:#F0F0F0;">';
+                echo '<strong>' . $k . "</strong></td><td>";
+                myprint_r($v);
+                echo "</td></tr>";
+        }
+        echo "</table>";
+        return;
+    }
+     echo $my_array;
 }
 
 ?>
