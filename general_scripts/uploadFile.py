399aab48c908c78c24fa58e2357c6be8b9d90833
erik <erikahndersun@gmail.com>
2018-12-10 09:36:20 -0500

    cleaning up deployment dir

--- general_scripts/uploadFile.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python
-#C:\Users\goath\AppData\Local\Programs\Python\Python37\python.exe
-import os, sys
-import cgi, cgitb
-path = "/c/xampp/htdocs/pnp/general_scripts/newDir"
-os.mkdir(path, 0755);
